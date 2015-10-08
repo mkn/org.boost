6dcf1aacca5c97ae2d726d3c7d208f05000717b9
Philip Deegan <pdeegan@datumize.com>
2015-10-08 08:49:37 +0200

    go

--- mkn.bat
+++ mkn.bat
@@ -1,6 +1,5 @@
 
 
-git clone --recursive http://github.com/boostorg/boost.git b
 cd b
 .\bootstrap
 .\b2 headers
--- mkn.sh
+++ mkn.sh
@@ -1,6 +1,5 @@
 
 
-git clone --recursive http://github.com/boostorg/boost.git b
 cd b
 ./bootstrap.sh
 ./b2 headers
