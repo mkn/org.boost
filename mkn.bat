453fbf7d7e4c5e01c003528a98b8e04d49ca9523
Philip Deegan <philip.deegan@gmail.com>
2015-12-18 23:52:41 +0000

    no bootstrap for windows

--- mkn.bat
+++ mkn.bat
@@ -1,9 +1,9 @@
 
 
 @ECHO off
-IF NOT EXIST b (
+SET PWD=%CD%
+IF NOT EXIST %PWD%\b (
 	git clone https://github.com/boostorg/boost -b master b --recursive
-	cd b
-	.\bootstrap
-	.\b2 headers
+	CD b
+	FOR /f %%d in ('dir /b %PWD%\b\libs') do IF EXIST %PWD%\b\libs\%%d\include ROBOCOPY /s %PWD%\b\libs\%%d\include\boost %PWD%\b\boost /NFL /NDL /NJH /NJS /nc /ns /np
 )
