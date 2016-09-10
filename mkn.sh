0fcf2dcb8b120fbdc3922ace01dc5d4530dfac4f
Dekken <philip.deegan@gmail.com>
2016-09-10 08:54:01 +0100

    Update mkn.sh

--- mkn.sh
+++ mkn.sh
@@ -1,7 +1,7 @@
 
 
 if [ ! -d "./b" ]; then
-	git clone https://github.com/boostorg/boost -b master b --recursive
+	git clone --depth 1 https://github.com/boostorg/boost -b master b --recursive
 	cd b
 	./bootstrap.sh
 	./b2 headers
