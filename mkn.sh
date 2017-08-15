6e39f9b2459cfe5ba583e6e06fa900735739192f
dekken <philip.deegan@gmail.com>
2017-08-15 23:17:34 +0100

    up

--- mkn.sh
+++ mkn.sh
@@ -1,9 +1,21 @@
+#!/usr/bin/env bash
 
+set -e
+
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 VERSION="master"
-if [ ! -d "./b" ]; then
-    git clone --depth 1 https://github.com/boostorg/boost -b $VERSION b --recursive
-    cd b
-    ./bootstrap.sh
-    ./b2 headers
-fi
+DIR="b"
+GIT_URL="https://github.com/boostorg/boost"
+
+HAS=1
+[ ! -d "$CWD/$DIR" ] && HAS=0 && git clone --depth 1 $GIT_URL -b $VERSION b --recursive
+
+pushd $DIR
+[ $HAS -eq 1 ] && git pull --recurse-submodules origin $VERSION
+
+rm -rf boost
+./bootstrap.sh
+./b2 headers
+
+popd
