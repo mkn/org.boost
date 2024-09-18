2368881bd9db1bec37afe58191e79995c7a848a7
PhilipDeegan <philip.deegan@gmail.com>
2024-09-18 13:34:16 +0200

    Next (#1)
    * consistent
    
    * actions

--- mkn.sh
+++ mkn.sh
@@ -8,18 +8,25 @@ BOOST="boost-${VER}"
 TAR_FILE="boost-${VER}-cmake.tar.xz"
 URL="https://github.com/boostorg/boost/releases/download/boost-${VER}/${TAR_FILE}"
 
+get(){
+  which wget && wget $URL || curl -Lo $TAR_FILE $URL
+}
+
 (
     cd $CWD
     [ -d "lib" ] && echo "boost already built - skipping" && exit 0
-    [ ! -f "$TAR_FILE" ] && wget $URL
+    [ ! -f "$TAR_FILE" ] && get
     [ ! -d "$BOOST" ] && tar xf $TAR_FILE
-    rm -rf build $DIR && mv "$BOOST" "$DIR"
-    mkdir build && cd build
-    cmake -G Ninja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX=$CWD \
-        -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true \
-        "$CWD/$DIR"
-    ninja && ninja install
+    rm -rf build $DIR && mv "$BOOST" "$DIR" && mkdir build
+    (
+        cd build
+        cmake -G Ninja \
+            -DCMAKE_BUILD_TYPE=Release  \
+            -DCMAKE_INSTALL_LIBDIR=lib  \
+            -DCMAKE_INSTALL_PREFIX=$CWD \
+            -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true \
+            "$CWD/$DIR"
+        ninja && ninja install
+    )
     rm -rf build $DIR $TAR_FILE
 )
